3a4198f47cba5c2d4d800f04b6224bf9ebeb0cfe
Adam Greig <adam@adamgreig.com>
2011-04-05 15:52:00 +0100

    first commit. seems to work.

---
+++ README.md
@@ -0,0 +1,9 @@
+pyautopull
+==========
+
+When hit, run a command. Used for autodeployment from a github commit hook.
+
+Adam Greig
+April 2011
+
+All code released into the public domain.
---
+++ pyautopull.py
@@ -0,0 +1,25 @@
+import json
+import subprocess
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+@app.route("/", methods=["POST"])
+def autodeploy():
+    try:
+        with open("./last_commit", "r") as f:
+            before = f.read()
+        after = json.loads(request.form["payload"])["after"]
+        if before != after:
+            rtn = subprocess.call(app.config["CMD"])
+            if rtn == 0:
+                with open("./last_commit", "w") as f:
+                    f.write(after)
+    except Exception as e:
+        print "exception: " + str(e)
+    finally:
+        return "thanks github, <3!\n"
+
+if __name__ == "__main__":
+    app.run()
